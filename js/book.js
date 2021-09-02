38e049aec22f666b28e94e0870ff5dda626d759c
tanvirahmed055 <ahmedtanvir6168@gmail.com>
2021-09-02 19:01:14 +0600

    fixed card sizing issue

--- js/book.js
+++ js/book.js
@@ -37,38 +37,34 @@ const bookCardDiv = url => {
         .then(data => {
             document.getElementById("spinner").classList.add("d-none");
             //console.log(data);
-            const first20Books = data.docs.slice(0, 20);
+            const first30Books = data.docs.slice(0, 30);
             const bookContainer = document.getElementById("book-items");
             const countContainer = document.getElementById("count-detail");
 
-            first20Books?.forEach(element => {
+            first30Books?.forEach(element => {
                 //console.log(element);
 
 
                 const bookDiv = document.createElement('div');
 
-                bookDiv.className = "col m-auto";
-                bookDiv.innerHTML = `<div class="m-3" >
-                <div class="card h-100  shadow rounded p-3">
-
-      
-                <img src="https://covers.openlibrary.org/b/id/${element?.cover_i ? element.cover_i : 10909258}-M.jpg" class="card-img-top img-fluid" alt="No Image Available"> 
-     
-                    <div class="card-body">
-                      <h6 class="card-title fw-bold">Book Name: ${element?.title || 'No Book Name Available'}</h6>
-                      <p class="card-text fw-bold">Author Name: ${element?.author_name?.slice(0, 10) || 'No Author Name Available'} </p>
-                      <p class="card-text fw-bold">Published Year: ${element?.first_publish_year || 'No Published Year Found'}</p>
-                      <p class="card-text fw-bold">Publisher Name: ${element?.publisher?.slice(0, 5) || 'No Publisher Name Found'}</p>
-                    </div>
-                  </div>
-                  </div>`
+                bookDiv.classList.add = "col";
+                bookDiv.innerHTML = `<div class="card h-100">
+                <img src="https://covers.openlibrary.org/b/id/${element?.cover_i ? element.cover_i : 10909258}-M.jpg" class="card-img-top" alt="...">
+                <div class="card-body">
+                  <h5 class="card-title">Book Name: ${element?.title}</h5>
+                  <p class="card-text">>Author Name: ${element?.author_name?.slice(0, 10)}</p>
+                  <p class="card-text">>Published Year: ${element?.first_publish_year}</p>
+                  <p class="card-text">>Publisher Name: ${element?.publisher?.slice(0, 5)}</p>
+                  
+                </div>
+              </div>`
                 bookContainer.appendChild(bookDiv);
             });
 
             if (data.numFound > 0) {
                 const searchCountDiv = document.createElement('div');
                 searchCountDiv.innerHTML = `
-            <div><h1 class="text-center">Showing first 20 results out of ${data.numFound} results<h1></div>
+            <div><h1 class="text-center">Showing first 30 results out of ${data.numFound} results<h1></div>
             `
                 countContainer.appendChild(searchCountDiv);
             } else {
